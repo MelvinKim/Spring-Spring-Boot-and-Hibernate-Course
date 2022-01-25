da6d6f036e12133e9f9b65446059b2f8015e69d0
Melvin Kimathi <melobruk1@gmail.com>
2022-01-25 07:13:16 +0300

    dependency_injection

--- src/com/luv2code/springdemo/HelloSpringApp.java
+++ src/com/luv2code/springdemo/HelloSpringApp.java
@@ -15,10 +15,33 @@ public class HelloSpringApp {
 		//now that i have the bean, i will call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		
+		//calling the new method for fortunes
+		System.out.println(theCoach.getDailyFortune());
+		
 		//close application context
 		context.close();
 	}
 
 }
 
+//implementation => provide an implementation for methods defined in the interface
+/*
+ 1.
+ <bean id="myFortuneService"
+ 	class = "com.luv2code.springdemo.HappyFortuneService">
+ </bean> 
+ --- what happens behind the scene ---
+ HappyFortuneService myFortuneService = new HappyFortuneService();
+ 
+ 2.<bean id="myCoach" 
+ 	class="com.luv2code.springdemo.BaseballCoach">
+ 	<constructor-arg ref="myFortuneService" />
+ 	</bean>
+ 	
+ --- what happens behind the scene ---
+ BaseballCoach myCoach = new BaseballCoach(myFortuneService);
+ 
+  */
 
+
+ 
--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -1,10 +1,27 @@
 package com.luv2code.springdemo;
 
 public class TrackCoach implements Coach {
+	
+	//update the TrackCoach to use the FortuneService
+	private FortuneService fortuneService;
+	
+	public TrackCoach() {
+		
+	}
+
+	//create constructor injection
+	public TrackCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		return "Run a hard 5k";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return "Just do It: " + fortuneService.getFortune();
 	}
 
 }
